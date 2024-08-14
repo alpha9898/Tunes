39df7282714ef6b720aff993f44b86d767566e79
‪Amar Yasser‬‏ <amaryasser046@gmail.com>
2024-08-14 17:53:19 +0300

    Enhance TuneView layout and add TuneItem widget for better visual structure. Update AppBar with title and styling, and populate body with multiple colorful TuneItem instances.

--- lib/views/tune_view.dart
+++ lib/views/tune_view.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/tune_item.dart';
+
 class TuneView extends StatelessWidget {
   const TuneView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Flutter Tune',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        elevation: 0,
+        backgroundColor: const Color(0xff253238),
+      ),
+      body: Column(
+        children: [
+          TuneItem(
+            color: Colors.cyan,
+          ),
+          TuneItem(
+            color: Colors.blue,
+          ),
+          TuneItem(
+            color: Colors.greenAccent,
+          ),
+          TuneItem(
+            color: Colors.redAccent,
+          ),
+          TuneItem(
+            color: Colors.brown,
+          ),
+          TuneItem(
+            color: Colors.purpleAccent,
+          ),
+          TuneItem(
+            color: Colors.cyan,
+          ),
+        ],
+      ),
     );
   }
 }
